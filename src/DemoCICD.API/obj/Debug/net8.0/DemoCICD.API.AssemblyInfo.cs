13b418bd855e830e84962bd7f2b631e091029dfa
hathanhdung <hathanhdung1995@gmail.com>
2024-01-25 21:03:08 +0700

    Commit

--- src/DemoCICD.API/obj/Debug/net8.0/DemoCICD.API.AssemblyInfo.cs
+++ src/DemoCICD.API/obj/Debug/net8.0/DemoCICD.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoCICD.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c86787957b95f665a583dffc5331d3a2a2d984f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoCICD.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoCICD.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
